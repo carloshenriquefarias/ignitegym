b61306b1bd081a7119594f13021a6aee9f72617a
Carlos Henrique <you@example.comcarloshenriquepvh@hotmail.com>
2022-12-14 17:28:25 -0400

    Construindo IgniteGym App

--- App.tsx
+++ App.tsx
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, StatusBar } from 'react-native';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 export default function App() {
+
+  const [fontsloaded] = useFonts({Roboto_400Regular, Roboto_700Bold})
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+    <View style={{flex: 1, justifyContent:"center", alignItems:"center", backgroundColor: '#202024'}}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      { fontsloaded ? <Text> E la vamos nos!</Text> : <View/> }      
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
